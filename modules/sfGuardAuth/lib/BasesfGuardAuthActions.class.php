83b132390838b785a82bbbd89059a49cab326d31
Joseph Rouff <rouffj@gmail.com>
2016-01-05 17:19:27 +0100

    apply feedbacks

--- modules/sfGuardAuth/lib/BasesfGuardAuthActions.class.php
+++ modules/sfGuardAuth/lib/BasesfGuardAuthActions.class.php
@@ -67,9 +67,7 @@ class BasesfGuardAuthActions extends sfActions
      $class = sfConfig::get('app_sf_guard_plugin_signin_form', 'sfGuardFormSignin');
      $this->form = new $class();
 
-     if ($request->isMethod('post')) {
-       $this->form->bind($request->getParameter('signin'));
-       if ($this->form->isValid()) {
+     if ($request->isMethod('post') && $this->form->bind($request->getParameter('signin'))->isValid()) {
          $values = $this->form->getValues();
          $this->getUser()->signin($values['user'], array_key_exists('remember', $values) ? $values['remember'] : false);
 
@@ -79,27 +77,25 @@ class BasesfGuardAuthActions extends sfActions
          $signinUrl = sfConfig::get('app_sf_guard_plugin_success_signin_url', $user->getReferer($request->getReferer()));
 
          return $this->redirect('' != $signinUrl ? $signinUrl : '@homepage');
-       }
      }
-     else {
-       if ($request->isXmlHttpRequest()) {
-         $this->getResponse()->setHeaderOnly(true);
-         $this->getResponse()->setStatusCode(401);
 
-         return sfView::NONE;
-       }
-
-       // if we have been forwarded, then the referer is the current URL
-       // if not, this is the referer of the current request
-       $user->setReferer($this->getContext()->getActionStack()->getSize() > 1 ? $request->getUri() : $request->getReferer());
-
-       $module = sfConfig::get('sf_login_module');
-       if ($this->getModuleName() != $module) {
-         return $this->redirect($module.'/'.sfConfig::get('sf_login_action'));
-       }
-
+     if ($request->isXmlHttpRequest()) {
+       $this->getResponse()->setHeaderOnly(true);
        $this->getResponse()->setStatusCode(401);
-      }
+
+       return sfView::NONE;
+     }
+
+     // if we have been forwarded, then the referer is the current URL
+     // if not, this is the referer of the current request
+     $user->setReferer($this->getContext()->getActionStack()->getSize() > 1 ? $request->getUri() : $request->getReferer());
+
+     $module = sfConfig::get('sf_login_module');
+     if ($this->getModuleName() != $module) {
+       return $this->redirect($module.'/'.sfConfig::get('sf_login_action'));
+     }
+
+     $this->getResponse()->setStatusCode(401);
   }
 
   private function ssoLogin()
